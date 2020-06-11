b8d9b89635b16e76867f0be67cbc9c4d42437196
huyang <hlhyly950117@gmail.com>
2020-06-11 22:04:35 +0800

    Add navigate-back function at checkout page

--- src/components/OrderPlacing/CheckoutSummary/CheckoutSummary.js
+++ src/components/OrderPlacing/CheckoutSummary/CheckoutSummary.js
@@ -11,8 +11,8 @@ const CheckoutSummary = (props) => {
             <div className={classes.Burger}>
                 <Burger ingredients={props.ingredients} />
             </div>
-            <Button btnType='Danger'>CANCEL</Button>
-            <Button btnType='Success'>CONTINUE</Button>
+            <Button btnType='Danger' clicked={props.checkoutCancelled}>CANCEL</Button>
+            <Button btnType='Success' clicked={props.checkoutContinued}>CONTINUE</Button>
         </div>
     );
 }
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,10 +11,22 @@ class Checkout extends Component {
             'meat':1
         }
     }
+
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
+        this.props.history.push('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary 
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler} />
             </div>
         );
     }
