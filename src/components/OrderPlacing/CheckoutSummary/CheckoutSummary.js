4c3c57d5ac3e3acacffc44e30db7968673b412b0
huyang <hlhyly950117@gmail.com>
2020-06-11 20:28:54 +0800

    Build the Checkout Container

---
+++ src/components/OrderPlacing/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './CheckoutSummary.css';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+const CheckoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope it tastes well!</h1>
+            <div className={classes.Burger}>
+                <Burger ingredients={props.ingredients} />
+            </div>
+            <Button btnType='Danger'>CANCEL</Button>
+            <Button btnType='Success'>CONTINUE</Button>
+        </div>
+    );
+}
+
+export default CheckoutSummary;
