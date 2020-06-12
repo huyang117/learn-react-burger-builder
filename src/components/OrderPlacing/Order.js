9142522aa2a8b8669d90e4583f5eecda6b2e7a8b
huyang <hlhyly950117@gmail.com>
2020-06-12 21:29:27 +0800

    Display the orders fetched from Firebase on the Orders page.

--- src/components/OrderPlacing/Order.js
+++ src/components/OrderPlacing/Order.js
@@ -3,12 +3,28 @@ import React from 'react';
 import classes from './Order.css';
 
 const Order = (props) => {
+    const ingredients = [];
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientsOutput = ingredients.map(ingredient => {
+        return (
+            <span key={ingredient.name} className={classes.Ingredient}>
+                {ingredient.name} ({ingredient.amount})
+            </span>);
+    })
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients: </p>
-            <p>Price: </p>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong></p>
+            <p>Delivery Method: {props.deliveryMethod}</p>
         </div>
     );
-} 
+}
 
 export default Order;
--- src/containers/HistoryOrders/HistoryOrders.js
+++ src/containers/HistoryOrders/HistoryOrders.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 import Order from '../../components/OrderPlacing/Order';
 
 class HistoryOrders extends Component {
     state = {
-        orders: null
+        orders: [],
+        loading: true
     }
 
     componentDidMount() {
         axios.get('/orders.json')
             .then(response => {
-                this.setState({ orders: response.data });
-                console.log(response.data);
+                const ordersFetched = [];
+                for (let key in response.data) {
+                    ordersFetched.push({
+                        ...response.data[key],
+                        id: key
+                    });
+                }
+                this.setState({
+                    orders: ordersFetched,
+                    loading: false
+                });
+                console.log(this.state.orders);
+            })
+            .catch(error => {
+                this.setState({ loading: false });
             });
     }
 
     render() {
+        let orders = <Spinner />;
+        if (!this.state.loading && this.state.orders.length===0) {
+            orders = <p>Order history is empty</p>;
+        }
+        else if (!this.state.loading && this.state.orders.length>0) {
+            orders = this.state.orders.map(order => (
+                <Order {...order} key={order.id} />
+            ));
+        }
         return (
             <div>
-                <Order />
+                {orders}
             </div>
         );
     }
 }
 
-export default HistoryOrders;
+export default withErrorHandler(HistoryOrders, axios);
